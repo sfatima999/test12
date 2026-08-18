50cb193203f85571a5a859eabe9021d51beb749a
Nidhi Singh <n.singh@innovise-it.com>
2026-08-18 12:22:42 +0530

    conflict

--- src/main/java/com/houarizegai/calculator/ui/CalculatorUI.java
+++ src/main/java/com/houarizegai/calculator/ui/CalculatorUI.java
@@ -588,5 +588,6 @@ public class CalculatorUI {
         btnEqual.setBackground(hex2Color(theme.getBtnEqualBackground()));
         btnEqual.setBackground(hex2Color(theme.getBtnEqualBackground()));
         btnEqual.setBackground(hex2Color(theme.getBtnEqualBackground()));
+        //conflict testing
     }
 }
